5f62761380f5e92db05e034cb4cd6240f1878f5c
Henry Bradlow <hbradlow@berkeley.edu>
2013-07-27 15:11:44 -0700

    Changed to use strings

--- server/solver/solver.py
+++ server/solver/solver.py
@@ -6,4 +6,4 @@ class Solver:
     def solve_simple(self,s):
         return sympy.sympify(s)
     def solve(self,s):
-        return self.solve_simple(s)
+        return str(self.solve_simple(s))
--- server/solver/solver_test.py
+++ server/solver/solver_test.py
@@ -8,7 +8,7 @@ class TestSequenceFunctions(unittest.TestCase):
         self.solver = Solver()
 
     def test_add(self):
-        self.assertTrue(self.solver.solve("1+6") == 7)
+        self.assertTrue(self.solver.solve("1+6") == "7")
 
 if __name__ == '__main__':
     unittest.main()
