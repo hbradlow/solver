75b5896248ce2b8705d99b6ff8edbdb63292c452
Henry Bradlow <hbradlow@berkeley.edu>
2013-07-27 15:37:10 -0700

    Added problem type detection

--- server/solver/solver.py
+++ server/solver/solver.py
@@ -1,9 +1,27 @@
 import sympy
+import wolframalpha
+
+NUMBERS = "1234567890"
+OPERATIONS = "=-+*%/"
 
 class Solver:
+
     def __init__(self):
-        pass
-    def solve_simple(self,s):
-        return sympy.sympify(s)
+        self.wolfram_client = wolframalpha.Client("XR8XP2-WXKLU74A3E")
+
+    def _wolfram(self,s):
+        res = self.wolfram_client.query(s)
+        for r in res:
+            title = r.node.attrib['title']
+            if title == "Solution" or title == "Result":
+                return r.node._children[0]._children[0].text
+        return get_answer(res)
+
+    def _simple(self,s):
+        return str(sympy.sympify(s))
+
     def solve(self,s):
-        return str(self.solve_simple(s))
+        for char in s:
+            if char not in NUMBERS and char not in OPERATIONS:
+                return self._wolfram(s)
+        return self._simple(s)
--- server/solver/solver_test.py
+++ server/solver/solver_test.py
@@ -7,8 +7,15 @@ class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
         self.solver = Solver()
 
-    def test_add(self):
-        self.assertTrue(self.solver.solve("1+6") == "7")
+    def test_random(self):
+        self.assertEquals(self.solver.solve("6*3"),"18")
+        self.assertEquals(self.solver.solve("x-4=10"),"x = 14")
+
+    def test_simple(self):
+        self.assertEquals(self.solver._simple("1+6"),"7")
+
+    def test_wolfram(self):
+        self.assertEquals(self.solver._wolfram("3x+4=10"),"x = 2")
 
 if __name__ == '__main__':
     unittest.main()
