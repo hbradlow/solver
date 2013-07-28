414dcf9559c8f0f5607fe814d51ec918af31839e
Henry Bradlow <hbradlow@berkeley.edu>
2013-07-27 20:19:14 -0700

    Added .png

--- server/parser/cropper.py
+++ server/parser/cropper.py
@@ -4,5 +4,5 @@ def crop(f, bbs):
     img = cv2.imread(f)
     i = 0
     for box in bbs:
-        cv2.imwrite("tmp"+str(i), img[x1:x2, y1:y2])
+        cv2.imwrite("tmp"+str(i)+".png", img[x1:x2, y1:y2])
         i += 1
